b608d3b7e967bb3859f0af8d6e40208de8c0ae1c
Tatsuki Nagaoka <nagaoka@mbp.local>
2020-06-28 12:25:16 +0900

    Add signup form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <router-view />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+const config = {
+  apiKey: 'AIzaSyCXFyD814y43adY79r5_AwqLbWVrJvFyZo',
+  authDomain: 'mysocialtippingproject.firebaseapp.com',
+  databaseURL: 'https://mysocialtippingproject.firebaseio.com',
+  projectId: 'mysocialtippingproject',
+  storageBucket: 'mysocialtippingproject.appspot.com',
+  messagingSenderId: '867786685702',
+  appId: '1:867786685702:web:8215ac995fc80e4703f287',
+  measurementId: 'G-4L64J2SQ5J',
+};
+firebase.initializeApp(config);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
